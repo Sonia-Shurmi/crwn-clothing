f4f5798668ba6eb08f19cc2f0209fbe6cca59476
Sonia Shurmi <soniashurmi@gmail.com>
2019-09-18 14:56:13 +0600

    updating shop page

---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,159 @@
+const SHOP_DATA = [
+    {
+        id: 1,
+        title: 'Hats',
+        routeName: 'hats',
+        items: [
+            {
+                id: 1,
+                name: 'Brown Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 25
+            },
+            {
+                id: 2,
+                name: 'Blue Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 18
+            },
+            {
+                id: 3,
+                name: 'Blue Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 18
+            },
+            {
+                id: 4,
+                name: 'Blue Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 18
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: 'jackets',
+        routeName: 'jackets',
+        items: [
+            {
+                id: 1,
+                name: 'Brown Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 25
+            },
+            {
+                id: 2,
+                name: 'Blue Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 18
+            },
+            {
+                id: 3,
+                name: 'Blue Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 18
+            },
+            {
+                id: 4,
+                name: 'Blue Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 18
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: 'sneakers',
+        routeName: 'sneakers',
+        items: [
+            {
+                id: 1,
+                name: 'Brown Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 25
+            },
+            {
+                id: 2,
+                name: 'Blue Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 18
+            },
+            {
+                id: 3,
+                name: 'Blue Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 18
+            },
+            {
+                id: 4,
+                name: 'Blue Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 18
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: 'womens',
+        routeName: 'womens',
+        items: [
+            {
+                id: 1,
+                name: 'Brown Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 25
+            },
+            {
+                id: 2,
+                name: 'Blue Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 18
+            },
+            {
+                id: 3,
+                name: 'Blue Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 18
+            },
+            {
+                id: 4,
+                name: 'Blue Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 18
+            }
+        ]
+    },
+    {
+        id: 5,
+        title: 'mens',
+        routeName: 'mens',
+        items: [
+            {
+                id: 1,
+                name: 'Brown Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 25
+            },
+            {
+                id: 2,
+                name: 'Blue Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 18
+            },
+            {
+                id: 3,
+                name: 'Blue Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 18
+            },
+            {
+                id: 4,
+                name: 'Blue Brim',
+                imageUrl: 'https:/i.ibb.co/R70vBrQ/mens.png',
+                price: 18
+            }
+        ]
+    }
+];
+
+export default SHOP_DATA;
